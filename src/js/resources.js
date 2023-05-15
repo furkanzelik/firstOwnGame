edab8f0e86fda31eee7fe2514e908a908a9f66b9
Huseyin Furkan Ozcelik <furkanzelik@gmail.com>
2023-05-15 18:14:31 +0300

    try

--- src/js/resources.js
+++ src/js/resources.js
@@ -1,9 +1,14 @@
 import { ImageSource, Sound, Resource, Loader } from 'excalibur'
-import fishImage from '../images/fish.png'
+// import fishImage from '../images/fish.png'
+import backgroundImage from '../images/castel_backround'
 
 const Resources = {
-    Fish: new ImageSource(fishImage)
+    // Fish: new ImageSource(fishImage)
+    Background: new ImageSource(backgroundImage)
 }
-const ResourceLoader = new Loader([Resources.Fish])
+const ResourceLoader = new Loader([
+    // Resources.Fish
+    Resources.Background
+])
 
 export { Resources, ResourceLoader }
